26d3674ca4eff0275fd3d9fa07b97aab6f57bb68
dipto1996 <roydiptopal1996@gmail.com>
2025-09-25 22:32:40 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // pageProps.session is passed automatically by NextAuth
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
